3d4dd8412155c6b5f86dd1f17f6f0592b67e30c1
liyengwas <liyengwas@gmail.com>
2023-08-01 22:06:08 +0300

    main.js function edits

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,62 @@
-// ... (existing code)
-
 const server = 'http://localhost:3000';
+var commodityId;
+var commodityName;
+var commodityPrice;
+
+// Fetch commodities and populate the table
+async function fetchCommodities() {
+
+    const url = server + '/commodities';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    const response = await fetch(url, options);
+    const commodities = await response.json();
+    populateContent(commodities);
+
+}
+
+// Add Commodity
+async function addcommodity() {
+    const url = `${server}/commodities`;
+    const commodity = { id: parseInt(commodityId), name: commodityName, price: commodityPrice };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(commodity)
+    };
+    const response = await fetch(url, options);
+}
+
+// Update Commodity
+async function updateCommodity() {
+    const url = `${server}/commodities/${commodityId}`;
+    const commodity = { name: commodityName, price: commodityPrice };
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(commodity)
+    };
+    const response = await fetch(url, options);
+}
+
+// Delete Commodity
+async function deleteCommodity() {
+    const url = `${server}/commodities/${commodityId}`;
+    const options = {
+        method: 'DELETE'
+    };
+    const response = await fetch(url, options);
+}
+
+
 
 // Function to handle the "View" button click event
 async function onViewButtonClick() {
@@ -13,7 +69,7 @@ async function onAddButtonClick() {
     commodityName = document.getElementById('commodityName').value;
 
     if (commodityId && commodityName) {
-        await addcommodity();
+        await addcomAmodity();
         await fetchCommodities();
     }
 }
@@ -36,53 +92,20 @@ async function onDeleteButtonClick() {
     if (commodityId) {
         await deleteCommodity();
         await fetchCommodities();
+        // Function to fetch commodities from the server and populate the table
+        async function fetchCommodities() {
+            // Fetch commodities from the server
+            const response = await fetch(`${server}/commodities`);
+            const commodities = await response.json();
+            populateContent(commodities);
+        }
     }
 }
 
-// Function to fetch commodities from the server and populate the table
-async function fetchCommodities() {
-    // Fetch commodities from the server
-    const response = await fetch(`${server}/commodities`);
-    const commodities = await response.json();
-    populateContent(commodities);
-}
 
-// Function to add a new commodity to the server
-async function addcommodity() {
-    const url = `${server}/commodities`;
-    const commodity = { id: parseInt(commodityId), name: commodityName };
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(commodity)
-    };
-    const response = await fetch(url, options);
-}
 
-// Function to update a commodity on the server
-async function updateCommodity() {
-    const url = `${server}/commodities/${commodityId}`;
-    const commodity = { name: commodityName };
-    const options = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(commodity)
-    };
-    const response = await fetch(url, options);
-}
 
-// Function to delete a commodity from the server
-async function deleteCommodity() {
-    const url = `${server}/commodities/${commodityId}`;
-    const options = {
-        method: 'DELETE'
-    };
-    const response = await fetch(url, options);
-}
+
 
 // ... (existing code)
 
