c10c49bbf2f45769b26a200cf66ff3690ca0c9b9
liyengwas <liyengwas@gmail.com>
2023-08-01 23:53:18 +0300

    rewrite index page

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 //app.use(express.static('public'));
 //app.use(express.static('public'));
+
 // Endpoint for fetching all commodities
 app.get('/commodities', (req, res) => {
   res.json(commodities);
